edea2caad9ad7255d9e8e69f57c8d059240ef05f
Guy Sandler <guy@sandler.co.il>
2023-08-23 18:07:39 -0700

    stuff

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+    import logo from '../lib/images/logo.svg';
+</script>
+
+<Navbar let:hidden let:toggle>
+    <NavBrand href="/">
+      <img src="{logo}" style="aspect-ratio:1/1;width:2vw;height:2vw;" alt="Flowbite Logo" />
+      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+    </NavBrand>
+    <NavHamburger on:click={toggle} />
+    <NavUl {hidden}>
+      <NavLi href="/">Home</NavLi>
+      <NavLi href="/about">About</NavLi>
+      <NavLi href="/docs/components/navbar">Navbar</NavLi>
+      <NavLi href="/pricing">Pricing</NavLi>
+      <NavLi href="/contact">Contact</NavLi>
+    </NavUl>
+</Navbar>
+<div>
+    <slot />
+</div>
+
+<style>
+    div {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+        font-family: ubuntu;
+    }
+</style>
